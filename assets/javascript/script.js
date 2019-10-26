0062652bd51b1ddf91960d76e67c3cff0f3f3128
Issa Issa <mrforesightii@gmail.com>
2019-10-26 12:11:44 -0500

    update to html and script

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,13 @@
 var id = "2c4dbd7e"
 var key = "307500e630ab60f1ee06b7febaaada1a"
-var inputVal = [];
-console.log(inputVal);
-​
-​
-​
+var inputVal = []
+console.log(inputVal)
+
+
+
 var queryURLrec = "https://api.edamam.com/search?q=" + inputVal[0] + "&app_id=$" + id + "&app_key=$" + key + "&from=0&to=3";
-​
-​
+
+
 function getRecipe() {
     $.ajax({
         type: "GET",
@@ -17,7 +17,8 @@ function getRecipe() {
         console.log(response);
     });
 }
-​
+
+
 $(function(){
     $('#save_value').click(function(){
       var val = [];
@@ -30,4 +31,5 @@ $(function(){
         getRecipe();
     });
   });
-​
+
+  
